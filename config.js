cbb6628b15c186c23267975d2936aec71b946e66
C O K L U K <47196492+cokluk@users.noreply.github.com>
2021-02-14 18:01:02 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,28 @@
+module.exports = class Config  {
+
+    constructor(){
+
+
+        //CO-FIVEM LISANS
+        this.lisans = "CO-FIVEM-130eca5e4e26639d3f66c8d4d4c5c6358ae9ba32";
+        this.test_mod = false;
+
+        //MYSQL - UZAK - LOCAL DESTEKLENIR
+        this.host = "localhost";
+        this.user = "root";
+        this.password = "";
+        this.database = "essentialmode";
+
+
+        //CO-FIVEM
+
+        this.prefix = "!";
+        this.prefix2 = "cofivem";
+        this.discord_token = "YOUR_DISCORD_TOKEN";
+        this.rol_izni =  "CO-FIVEM";
+ 
+ 
+    }
+
+
+}
